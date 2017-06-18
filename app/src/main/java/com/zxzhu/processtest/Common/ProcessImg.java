36c82f30118df619f522627da31a094233d427c5
zxzhu <354328995@qq.com>
2017-06-18 21:14:01 +0800

    设置属性方法顺序随意

--- app/src/main/java/com/zxzhu/processtest/Common/ProcessImg.java
+++ app/src/main/java/com/zxzhu/processtest/Common/ProcessImg.java
@@ -21,16 +21,17 @@ import java.util.List;
 
 public class ProcessImg extends LinearLayout {
     private String TAG = "ProcessImg";
-    private CircleView circleView1,circleView2,circleView3,circleView4,circleView5,circleView6;
-    private TextView textView1,textView2,textView3,textView4,textView5,textView6;
-    private View view1l,view1r,view2l,view2r,view3l,view3r,view4l,view4r,view5l,view5r,view6l,view6r;
-    private LinearLayout process1,process2,process3,process4,process5,process6;
+    private CircleView circleView1, circleView2, circleView3, circleView4, circleView5, circleView6;
+    private TextView textView1, textView2, textView3, textView4, textView5, textView6;
+    private View view1l, view1r, view2l, view2r, view3l, view3r, view4l, view4r, view5l, view5r, view6l, view6r;
+    private LinearLayout process1, process2, process3, process4, process5, process6;
     private List<LinearLayout> layouts = new ArrayList<>();
     private List<View> views = new ArrayList<>();
     private List<TextView> texts = new ArrayList<>();
     private List<CircleView> circleViews = new ArrayList<>();
     private Context context;
     private int color = Color.parseColor("#E0E0E0");
+    private int total = 0, process = 0;
 
     public ProcessImg(Context context) {
         super(context);
@@ -39,33 +40,33 @@ public class ProcessImg extends LinearLayout {
         initViews();
     }
 
-    public ProcessImg(Context context, AttributeSet attributeSet){
-        super(context,attributeSet);
+    public ProcessImg(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
         this.context = context;
         Log.d(TAG, "ProcessImg: 调用构造函数");
         initViews();
     }
 
-    private void initViews(){
-        LayoutInflater.from(context).inflate(R.layout.process_img,this);
-        circleView1 = (CircleView)findViewById(R.id.circle1);
-        circleView2 = (CircleView)findViewById(R.id.circle2);
-        circleView3 = (CircleView)findViewById(R.id.circle3);
-        circleView4 = (CircleView)findViewById(R.id.circle4);
-        circleView5 = (CircleView)findViewById(R.id.circle5);
-        circleView6 = (CircleView)findViewById(R.id.circle6);
-        process1 = (LinearLayout)findViewById(R.id.process1);
-        process2 = (LinearLayout)findViewById(R.id.process2);
-        process3 = (LinearLayout)findViewById(R.id.process3);
-        process4 = (LinearLayout)findViewById(R.id.process4);
-        process5 = (LinearLayout)findViewById(R.id.process5);
-        process6 = (LinearLayout)findViewById(R.id.process6);
-        textView1 = (TextView)findViewById(R.id.text1);
-        textView2 = (TextView)findViewById(R.id.text2);
-        textView3 = (TextView)findViewById(R.id.text3);
-        textView4 = (TextView)findViewById(R.id.text4);
-        textView5 = (TextView)findViewById(R.id.text5);
-        textView6 = (TextView)findViewById(R.id.text6);
+    private void initViews() {
+        LayoutInflater.from(context).inflate(R.layout.process_img, this);
+        circleView1 = (CircleView) findViewById(R.id.circle1);
+        circleView2 = (CircleView) findViewById(R.id.circle2);
+        circleView3 = (CircleView) findViewById(R.id.circle3);
+        circleView4 = (CircleView) findViewById(R.id.circle4);
+        circleView5 = (CircleView) findViewById(R.id.circle5);
+        circleView6 = (CircleView) findViewById(R.id.circle6);
+        process1 = (LinearLayout) findViewById(R.id.process1);
+        process2 = (LinearLayout) findViewById(R.id.process2);
+        process3 = (LinearLayout) findViewById(R.id.process3);
+        process4 = (LinearLayout) findViewById(R.id.process4);
+        process5 = (LinearLayout) findViewById(R.id.process5);
+        process6 = (LinearLayout) findViewById(R.id.process6);
+        textView1 = (TextView) findViewById(R.id.text1);
+        textView2 = (TextView) findViewById(R.id.text2);
+        textView3 = (TextView) findViewById(R.id.text3);
+        textView4 = (TextView) findViewById(R.id.text4);
+        textView5 = (TextView) findViewById(R.id.text5);
+        textView6 = (TextView) findViewById(R.id.text6);
         view1l = findViewById(R.id.view1l);
         view1r = findViewById(R.id.view1r);
         view2l = findViewById(R.id.view2l);
@@ -112,55 +113,62 @@ public class ProcessImg extends LinearLayout {
 
     /**
      * 设置已完成流程颜色
+     *
      * @param color
      */
-    public void setColor(int color){
+    public void setColor(int color) {
         this.color = color;
+        setProcess(total, process);
     }
 
     /**
      * 设置进度
-     * @param total 总共流程个数(不超过6)
+     *
+     * @param total   总共流程个数(不超过6)
      * @param process 当前进度
      */
-    public void setProcess(int total,int process){
-        for (int i = 0 ; i<total ; i++){
+    public void setProcess(int total, int process) {
+        this.total = total;
+        this.process = process;
+        for (int i = 0; i < total; i++) {
             layouts.get(i).setVisibility(VISIBLE);
         }
-        views.get(0).setBackgroundColor(Color.parseColor("#00000000"));
-        views.get(2*total-1).setBackgroundColor(Color.parseColor("#00000000"));
-        for (int i = 0 ; i<circleViews.size() ; i++){
-            if (i<process){
+        for (int i = 0; i < circleViews.size(); i++) {
+            if (i < process) {
                 circleViews.get(i).setFillColor(color);
                 circleViews.get(i).setBackgroundColor(color);
                 circleViews.get(i).setStrokeColor(color);
                 texts.get(i).setTextColor(color);
 
-            }else {
+            } else {
                 circleViews.get(i).setFillColor(Color.parseColor("#CCCCCC"));
                 circleViews.get(i).setBackgroundColor(Color.parseColor("#CCCCCC"));
-                texts.get(i).setTextColor(Color.parseColor("#CCCCCC"));                circleViews.get(i).setStrokeColor(color);
+                texts.get(i).setTextColor(Color.parseColor("#CCCCCC"));
+                circleViews.get(i).setStrokeColor(color);
                 circleViews.get(i).setStrokeColor(Color.parseColor("#CCCCCC"));
             }
         }
-        for (int i = 0 ; i<views.size() ; i++){
-            if (i<process*2){
+        for (int i = 0; i < views.size(); i++) {
+            if (i < process * 2) {
                 views.get(i).setBackgroundColor(color);
-            }else {
+            } else {
                 views.get(i).setBackgroundColor(Color.parseColor("#CCCCCC"));
             }
         }
         views.get(0).setBackgroundColor(Color.parseColor("#00000000"));
-        views.get(2*total-1).setBackgroundColor(Color.parseColor("#00000000"));
+        if (total != 0) {
+            views.get(2 * total - 1).setBackgroundColor(Color.parseColor("#00000000"));
+        }
     }
 
     /**
      * 设置各进度标题
+     *
      * @param position
      * @param text
      */
-    public void setTitle(int position , String text){
-        texts.get(position-1).setText(text);
+    public void setTitle(int position, String text) {
+        texts.get(position - 1).setText(text);
     }
 
 
